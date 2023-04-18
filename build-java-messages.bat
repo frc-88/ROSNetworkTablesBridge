6f68469d696122e62bfe0b0d4d986d99b5e08953
Woz4tetra <woz4tetra@gmail.com>
2023-04-18 03:46:55 -0400

    fix path

--- build-java-messages.bat
+++ build-java-messages.bat
@@ -3,5 +3,5 @@
 SET BASE_DIR=%~dp0
 
 SET GEN_DIR=%BASE_DIR%\ros_networktables_bridge_genmsg
-call "%BASE_DIR%\venv\bin\activate.bat"
+call "%GEN_DIR%\venv\Scripts\activate"
 python "%GEN_DIR%\main.py" "-r" "src/main/java" "-m" "frc/team88/ros/messages"
