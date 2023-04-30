131c630b2a20137a84c1939a8f2f7030ccc5c9fa
Woz4tetra <woz4tetra@gmail.com>
2023-04-30 11:18:53 -0400

    make pairs hashable

--- src/main/java/frc/team88/ros/Pair.java
+++ src/main/java/frc/team88/ros/Pair.java
@@ -4,13 +4,17 @@
 
 package frc.team88.ros;
 
+import java.util.Objects;
+
 public class Pair<A, B> {
   private final A m_first;
   private final B m_second;
+  private final int m_hashCode;
 
   public Pair(A first, B second) {
     m_first = first;
     m_second = second;
+    m_hashCode = Objects.hash(m_first, m_second);
   }
 
   public A getFirst() {
@@ -23,5 +27,22 @@ public class Pair<A, B> {
 
   public static <A, B> Pair<A, B> of(A a, B b) {
     return new Pair<>(a, b);
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    Pair<?, ?> other_pair = (Pair<?, ?>) other;
+    return this.m_first.equals(other_pair.m_first) && this.m_second.equals(other_pair.m_second);
+  }
+
+  @Override
+  public int hashCode() {
+    return this.m_hashCode;
   }
 }
